a97a619f85281f072af64b5dea687889eaee54fe
Jon Liew <jon.l2147@hotmail.com>
2020-03-17 18:14:46 +1100

    Bear the first and last child, before the breakpoint

--- src/App.js
+++ src/App.js
@@ -7,22 +7,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">      
+        <h1>I am the gift of Averil Machin.</h1>
+        <div className="header-links">
+          <a className="header-link" href="">Interaction</a>
+          <a className="header-link" href="">Print</a>
+          <a className="header-link" href="">Motion</a>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+/*
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+*/
--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const myFirstElement = <h1>I am the gift of Averil Machin.</h1>
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(myFirstElement, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
