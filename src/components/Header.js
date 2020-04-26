0c6d4dc2f0e6d6a27456d837bc822a3580cec83e
Jon Liew <jon.l2147@hotmail.com>
2020-04-26 18:31:22 +1000

    Assemble header fundamentals, preceding file loader

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+
+import {ReactComponent as UnimelbLogo} from './images/unimelb.svg';
+
+
+
+
 class Header extends React.Component {
+
+
+
+    state = {
+        profs: ["Photoshop","InDesign","Maya","Mudbox","Illustrator","Acrobat","Cinema4D","Figma","Sketch",
+        "Principle","Sony Vegas","Premiere","After Effects","Visual Studio Code","Android Studio","Unity"]
+    }
+
+
     render() {
+
+        const { profs } = this.state
+
         return (
             
-            <header className="folio-header">
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-                
-
-                <div className="header-bio">
-                    <h1 className="bio-label">
-                        Jonathan Liew
-                    </h1>
-                    <p className="bio-description">Si eth e burion nal things as raubriel ro sorriend. This is a space where things I've designed might go. Her o sibileth araubries malmathras nwenverbethal.</p>
-                    <div class="bio-contact">
-                        <p>Send me an email if you'd like to get in touch</p>
-                        <a className="bio-contact-link" href="mailto:design@jlliew.com">design@jlliew.com</a>
-                    </div>
-                </div>
-                <div className="header-divider"/>
-                <div className="header-auxiliary">
-                    <div className="recent-experience">
-                    
-                        <h2 className="auxiliary-label">
-                            Recent experience
-                        </h2>
-                        <div className="experiences">
-                            <div className="experience"></div>
+            <header className="folio-header">                
+                <div className="header-container">
+                    <div className="header-bio">
+                        <h1 className="bio-label">
+                            Jonathan Liew
+                        </h1>
+                        <p className="bio-description">
+                            Si eth e burion nal things as raubriel ro sorriend. This is a space where things I've designed might go. Her o sibileth araubries malmathras nwenverbethal.
+                        </p>
+                        <div class="bio-contact">
+                            <p>Send me an email if you'd like to get in touch</p>
+                            <a className="bio-contact-link" href="mailto:design@jlliew.com">design@jlliew.com</a>
                         </div>
                     </div>
-                    <div className="view-work-link">
-                        View Projects
-                        <i class="material-icons">&#xe5db;</i>
+                    <div className="header-divider"/>
+                    <div className="header-auxiliary">
+                        <div className="recent-experience">                    
+                            <h2 className="auxiliary-label">
+                                Recent experience
+                            </h2>
+                            <div className="experiences">
+                                <div className="experience">
+                                    <UnimelbLogo 
+                                    alt="Digital Designer, University of Melbourne"
+                                    width="3rem" height="auto"
+                                     />
+                                    {/*<img src={UnimelbLogo}
+                                        alt="Digital Designer, University of Melbourne"
+                                        className="experience-graphic">
+                                    </img>*/}
+                                    <div className="experience-position">Digital Designer</div>
+                                    <div className="experience-workplace">University of Melbourne</div>
+                                </div>
+                                <div className="experience">
+                                    <div className="experience-graphic"></div>
+                                    <div className="experience-position">Graphic Designer</div>
+                                    <div className="experience-workplace">Pictoria Media</div>
+                                </div>
+                                <div className="experience">
+                                    <div className="experience-graphic"></div>
+                                    <div className="experience-position">Freelance Artist</div>
+                                    <div className="experience-workplace">@WartideCCG</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a className="projects-link" href="google.com">
+                            View Projects
+                            <ArrowDownward className="projects-icon"/>
+                        </a>
+                        <div className="software-proficiencies">
+                            <h2 className="auxiliary-label">
+                                Software proficiency
+                            </h2>
+                            <div className="proficiencies">
+                                {profs.map(src => {
+                                    return <span
+                                        className="proficiency"
+                                        key={src} src={src}>
+                                            {src}
+                                        </span>;
+                                })}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </header>
