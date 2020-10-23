5efbcf061c2f45de63a55a02a3a5b7e4e1183a68
Jon Liew <jon.l2147@hotmail.com>
2020-10-23 21:33:16 +1100

    Fire that base, but also follow it up with separating the Randomotron data from the class

--- src/components/Randomotron.js
+++ src/components/Randomotron.js
@@ -2,18 +2,10 @@ import React, {useState, useEffect} from 'react';
 import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import './Randomotron.css';
 
-const items = [
-    (<span>Some sound dishes I've been enjoying: <a className="link" target="_blank" href="https://open.spotify.com/playlist/2422RGEkrULJt2cVXEBg9t?si=fI3OdOYdT-qJ9YWDstW5jQ">
-    Spotify ♫
-        </a>
-    </span>),
-    (<div>Cooking tip #41: if uncertain, add <i>more</i> butter.</div>),
-/*(<span>
-        This is <i>my</i> favourite <a className="App-link" target="_blank" href="https://commons.wikimedia.org/wiki/File:L%C3%B3magn%C3%BA_with_sand_blowing.jpg">
-            big rock
-            <OpenInNewIcon className="randomotron-icon"/></a>.
-    </span>)*/
-];
+import { randomotronItems } from '../data/randomotron-items.js'
+
+
+const items = randomotronItems;
 
 const showInterval = 4800;
 const hideInterval = 2400;
---
+++ src/data/randomotron-items.js
@@ -0,0 +1,14 @@
+import React from 'react' // React must be in scope to use JSX
+
+export const randomotronItems =  [
+    (<span>Some sound dishes I've been enjoying: <a className="link" target="_blank" href="https://open.spotify.com/playlist/2422RGEkrULJt2cVXEBg9t?si=fI3OdOYdT-qJ9YWDstW5jQ">
+    Spotify ♫
+        </a>
+    </span>),
+    (<div>Cooking tip #41: if uncertain, add <i>more</i> butter.</div>),
+/*(<span>
+        This is <i>my</i> favourite <a className="App-link" target="_blank" href="https://commons.wikimedia.org/wiki/File:L%C3%B3magn%C3%BA_with_sand_blowing.jpg">
+            big rock
+            <OpenInNewIcon className="randomotron-icon"/></a>.
+    </span>)*/
+];
