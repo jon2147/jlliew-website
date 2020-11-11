cc5635c4c51fd383b0cac491cf2e56d4d982096e
Jon Liew <jon.l2147@hotmail.com>
2020-11-11 18:43:22 +1100

    Set initial full round of XL breakpoint sizes

--- src/components/OverlayGallery.js
+++ src/components/OverlayGallery.js
@@ -67,7 +67,7 @@ class OverlayGallery extends React.Component {
                 {/* Carousel navigation; use style={{ display: showNav ? "flex" : "none" }} to hide nav for single-image project */}
                 <span className={showNav ? "carousel-nav" : "carousel-nav single"} >
                     <a className="carousel-nav-direction" style={{ display: showNav ? "flex" : "none" }}>
-                        <ArrowBack />
+                        <ArrowBack className="carousel-nav-direction-icon"/>
                     </a>
                     <span className="carousel-nav-position">
                         {galleryData.map((image, index) => {
@@ -78,7 +78,7 @@ class OverlayGallery extends React.Component {
                         })}
                     </span>
                     <a className="carousel-nav-direction forward" style={{ display: showNav ? "flex" : "none" }} >
-                        <ArrowForward />
+                        <ArrowForward className="carousel-nav-direction-icon" />
                     </a>
                 </span>
             </div >
