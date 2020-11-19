b9ed720c2091d18018ad5416fc49188238864869
Jon Liew <jon.l2147@hotmail.com>
2020-11-19 17:39:02 +1100

    Add FTDE teaser to randomotron

--- src/data/randomotron-items.js
+++ src/data/randomotron-items.js
@@ -5,7 +5,11 @@ import Launch from '@material-ui/icons/Launch';
 export const randomotronItems =  [
     (<span>Some sound dishes I've been enjoying: <a className="link" target="_blank" title="Spotify playlist: Heidelberg Hymnary" rel="noopener noreferrer" href="https://open.spotify.com/playlist/2422RGEkrULJt2cVXEBg9t?si=fI3OdOYdT-qJ9YWDstW5jQ">
     Spotify ♫</a></span>),
-    (<div>Cooking tip #14: if uncertain, add <i>more</i> butter.</div>),    
+    (<div>Cooking tip #14: if uncertain, add <i>more</i> butter.</div>),
+    (<span>
+        An inaugural sketch for a series of <a className="link" target="_blank" title="Buttered Prawn with Parsley" href="/images/redbubble/foods_that_dont_exist.jpg">
+            Foods that Don't Exist<Launch className="randomotron-icon"/></a>
+    </span>),    
     (<span>
         This is my favourite <a className="link" target="_blank" title="Lómagnú with sand blowing over Skeiðarársandur" rel="noopener noreferrer"  href="https://upload.wikimedia.org/wikipedia/commons/a/a4/L%C3%B3magn%C3%BA_with_sand_blowing.jpg">
             big rock<Launch className="randomotron-icon"/></a>.
