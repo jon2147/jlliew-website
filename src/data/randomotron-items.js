a7c28589ccd09b9475b605d814f0df10c4b55596
Jon Liew <jon.l2147@hotmail.com>
2020-11-20 20:41:38 +1100

    Add Suspiria study to randomotron

--- src/data/randomotron-items.js
+++ src/data/randomotron-items.js
@@ -6,9 +6,13 @@ export const randomotronItems =  [
     (<span>Some sound dishes I've been enjoying: <a className="link" target="_blank" title="Spotify playlist: Heidelberg Hymnary" rel="noopener noreferrer" href="https://open.spotify.com/playlist/2422RGEkrULJt2cVXEBg9t?si=fI3OdOYdT-qJ9YWDstW5jQ">
     Spotify ♫</a></span>),
     (<div>Cooking tip #14: if uncertain, add <i>more</i> butter.</div>),
+    ((<span>
+        I'm trying to get back into <a className="link" target="_blank" title="Suspiria Sabbath Study" href="/images/randomotron/suspiria_sabbath_study_bw.jpg">
+            daily studies<Launch className="randomotron-icon"/></a> again!
+    </span>)),
     (<span>
-        An inaugural sketch for a series of <a className="link" target="_blank" title="Buttered Prawn with Parsley" href="/images/redbubble/foods_that_dont_exist.jpg">
-            Foods that Don't Exist<Launch className="randomotron-icon"/></a>
+        Here's an <a className="link" target="_blank" title="Buttered Prawn with Parsley" href="/images/redbubble/foods_that_dont_exist.jpg">
+            inaugural sketch<Launch className="randomotron-icon"/></a> for a series of <i>Foods that Don't Exist</i>.
     </span>),    
     (<span>
         This is my favourite <a className="link" target="_blank" title="Lómagnú with sand blowing over Skeiðarársandur" rel="noopener noreferrer"  href="https://upload.wikimedia.org/wikipedia/commons/a/a4/L%C3%B3magn%C3%BA_with_sand_blowing.jpg">
